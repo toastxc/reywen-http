963b6ed94a56fb90a5d4951d47584d576071b575
ToastXC <100072983+toastxc@users.noreply.github.com>
2023-07-26 09:27:31 +0800

    added option for encoding url

--- src/utils.rs
+++ src/utils.rs
@@ -38,7 +38,7 @@ pub fn struct_to_url_vlod<T: Serialize>(query: T) -> String {
 /// converts the data of a structure into url query parameters
 /// this function does not support objects  
 #[cfg(feature = "serde")]
-pub fn struct_to_url<T: Serialize>(query: T) -> String {
+pub fn struct_to_url<T: Serialize>(query: T, encode: bool) -> String {
     serde_json::from_str::<serde_json::Map<String, serde_json::Value>>(
         &serde_json::to_string(&query).unwrap_or_default(),
     )
@@ -47,27 +47,31 @@ pub fn struct_to_url<T: Serialize>(query: T) -> String {
     .filter_map(|(key, value)| {
         // arrays
         if let Some(array) = value.as_array() {
+            let mut counter = 0;
+
             Some(
                 array
                     .iter()
-                    .map(|item| format!("{key}[]={}", encode_value(item)))
+                    .map(|item| {
+                        let count_value = if counter == 0 { "" } else { "&" };
+                        counter += 1;
+                        format!("{}{key}[]={}", count_value, encode_value(item, encode))
+                    })
                     .collect(),
             )
+
             // blacklist - non supported types
         } else if value.is_object() | value.is_null() {
             None
         } else {
             // standard value parse
-            Some(format!("{key}={}", encode_value(&value)))
+            Some(format!("{key}={}", encode_value(&value, encode)))
         }
     })
     .collect::<Vec<String>>()
     .into_iter()
     .enumerate()
-    .map(|(index, item)| {
-        println!("ITEM: {:#?}", &item);
-        (if index == 0 { "?" } else { "&" }).to_string() + &item
-    })
+    .map(|(index, item)| (if index == 0 { "?" } else { "&" }).to_string() + &item)
     .collect::<String>()
 }
 
@@ -93,7 +97,8 @@ async fn test_url() {
     };
     // testing different implementations for url encoding
     println!("ORIGINAL: {url}, {:?}", data);
-    println!("TOAST ENCODED: {url}{}", struct_to_url(&data));
+    println!("TOAST ENCODED: {url}{}", struct_to_url(&data, false));
+    println!("TOAST ENCODED ENABLED: {url}{}", struct_to_url(&data, true));
     println!("VLOD ENCODED: {url}{}", struct_to_url_vlod(&data));
 }
 
@@ -101,10 +106,14 @@ pub fn if_false(t: &bool) -> bool {
     !t
 }
 #[cfg(feature = "serde")]
-pub fn encode_value(value: &serde_json::Value) -> String {
-    encode_str(&value.to_string())
+pub fn encode_value(value: &serde_json::Value, encode: bool) -> String {
+    encode_str(&value.to_string(), encode)
 }
 
-pub fn encode_str(value: &str) -> String {
-    urlencoding::encode(value).into_owned()
+pub fn encode_str(value: &str, encode: bool) -> String {
+    if encode {
+        urlencoding::encode(value).into_owned()
+    } else {
+        value.to_owned()
+    }
 }
